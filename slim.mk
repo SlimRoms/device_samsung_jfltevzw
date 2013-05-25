c91291b347cb3c988c69349fe62f51460e723d88
cordell <cordellsmail@gmail.com>
2013-05-25 12:10:08 +0200

    Slim bring up of jfltevzw

---
+++ slim.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := jfltevzw
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/cdma.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_sgs.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+#bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := slim_jfltevzw
+PRODUCT_DEVICE := jfltevzw
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SCH-I545
+PRODUCT_MANUFACTURER := Samsung
