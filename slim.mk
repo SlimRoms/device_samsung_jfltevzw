666d82161c06e5300fe91794907dea2f1bccc23e
cordell <cordellsmail@gmail.com>
2013-12-17 22:57:47 +0100

    update fingerprint
    Change-Id: I2159abf4582c60dfb39a015432a0fc013fffe0d3

--- slim.mk
+++ slim.mk
@@ -13,7 +13,10 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+  BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMK2:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltevzw-user 4.3 JSS15J I545VRUEMK2 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := slim_jfltevzw
