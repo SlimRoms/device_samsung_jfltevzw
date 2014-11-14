cf4abb186da4f650d741e90289174e60127c988c
Dan Pasanen <dan.pasanen@gmail.com>
2014-11-14 16:02:20 -0600

    jfltevzw: de-commonize loki
    Change-Id: Ieacd6e57657512bb18457890dac93a264f642f9c
    
    Conflicts:
    	BoardConfig.mk
    	device.mk

--- slim.mk
+++ slim.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := jfltevzw
-
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/cdma.mk)
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMK2:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.4.3 JSS15J I545VRUEMK2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltevzw \
+  TARGET_DEVICE=jfltevzw \
+  BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.3/JSS15J/I545VRUEMK2:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltevzw-user 4.4.3 JSS15J I545VRUEMK2 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
